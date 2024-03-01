8862c78cc7566f65423a384accca81701f885be6
balu83vs <balu83vs@gmail.com>
2024-03-01 07:29:34 +0300

    db_conn - done

--- db_con.py
+++ db_con.py
@@ -2,23 +2,24 @@ from pymongo import MongoClient
 
 # класс взаимодействия с базой данных mongodb
 class DbConnector:
-    def __init__(self, host='localhost', port=27017, db_name='newdb', collection_name='sample_collection'):
+    def __init__(self, host='localhost', port=27017, 
+                 db_name='newdb', collection_name='sample_collection'):
         self.host = host
-        self. port = port 
+        self.port = port 
         self.db_name = db_name
         self.collection_name = collection_name
 
-    # установка соединения
+    # установка соединения (подключение клиента)
     def add_client(self):
         client = MongoClient(host=self.host, port=self.port)    
         return client
     
-    # выбор базы данных
+    # выбор базы данных (подключение БД)
     def start_connect(self):
         db = self.add_client()[self.db_name]
         return db
     
-    # выбор коллекции
+    # выбор коллекции (выбор необходимой коллекции)
     def choice_collection(self):
         db_collection = self.start_connect()[self.collection_name]
         return db_collection
